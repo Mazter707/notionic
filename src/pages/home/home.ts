a18bb5cc92c295b05c091b97ceb40ebff05fd4d4
Mazter707 <mazter707@gmail.com>
2017-09-07 19:15:54 -0700

    Probando Lista y cambio de vista

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NotesService } from '../../services/notes.service';
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  notes = [];
+  constructor(public navCtrl: NavController, public notesService : NotesService) {
+    this.notes = notesService.getNotes();
   }
 
+    public goToDetail(){
+      this.navCtrl.push(DetailPage);
+    }
 }
