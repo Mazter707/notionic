ed5e1fb6a634fd99570c18a9414d342194a7e3bb
Mazter707 <mazter707@gmail.com>
2017-09-09 14:12:29 -0700

    first commit
    first

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -15,6 +15,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailPage {
 
+  note={id:null, title:'Prueba', description:null}
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
